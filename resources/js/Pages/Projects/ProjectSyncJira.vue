49fc491902f3c1d7ead353d49b1d91d78c3f6fd2
Son <son@chillbits.com>
2021-08-08 12:38:18 +0700

    Add import jira section again

--- resources/js/Pages/Projects/ProjectSyncJira.vue
+++ resources/js/Pages/Projects/ProjectSyncJira.vue
@@ -6,11 +6,11 @@
             <!-- Import Project From Jira -->
             <jet-form-section @submitted="syncJiraProject">
                 <template #title>
-                    Sync Project From Jira
+                    Import Project From Jira
                 </template>
 
                 <template #description>
-                    Sync data from Jira like projects and issues.
+                    Import data from Jira like projects and issues.
                 </template>
 
                 <!-- Import Project Form -->
@@ -44,37 +44,38 @@
 </template>
 
 <script>
-    import JetFormSection from '@/Jetstream/FormSection'
-    import JetSectionBorder from '@/Jetstream/SectionBorder'
-    import JetLabel from '@/Jetstream/Label'
-    import JetInput from '@/Jetstream/Input'
-    import JetInputError from '@/Jetstream/InputError'
-    import JetButton from '@/Jetstream/Button'
-    import JetActionMessage from '@/Jetstream/ActionMessage'
-
-    export default {
-        components: {
-            JetFormSection,
-            JetSectionBorder,
-            JetLabel,
-            JetInput,
-            JetInputError,
-            JetButton,
-            JetActionMessage
-        },
-
-        data() {
-            return {
-                syncJiraProjectForm: this.$inertia.form({
-                    jira_key: '',
-                }),
-            }
-        },
-
-        methods: {
-            syncJiraProject() {
-
-            }
-        },
-    }
+import JetFormSection from '@/Jetstream/FormSection'
+import JetSectionBorder from '@/Jetstream/SectionBorder'
+import JetLabel from '@/Jetstream/Label'
+import JetInput from '@/Jetstream/Input'
+import JetInputError from '@/Jetstream/InputError'
+import JetButton from '@/Jetstream/Button'
+import JetActionMessage from '@/Jetstream/ActionMessage'
+export default {
+    components: {
+        JetFormSection,
+        JetSectionBorder,
+        JetLabel,
+        JetInput,
+        JetInputError,
+        JetButton,
+        JetActionMessage
+    },
+    data() {
+        return {
+            syncJiraProjectForm: this.$inertia.form({
+                jira_key: '',
+            }),
+        }
+    },
+    methods: {
+        syncJiraProject() {
+            this.syncJiraProjectForm.post(route('projects.sync.jira'), {
+                errorBag: 'syncJiraProject',
+                preserveScroll: true,
+                onSuccess: () => this.syncJiraProjectForm.reset(),
+            });
+        }
+    },
+}
 </script>
