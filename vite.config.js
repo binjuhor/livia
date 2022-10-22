8f0837bb3bb436f4a5734df7ba4947a3d14b2243
Son Do <son@chillbits.com>
2022-10-22 12:14:37 +0000

    A BIG commit today: Upgrade L9 and J2

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: 'resources/js/app.js',
+            ssr: 'resources/js/ssr.js',
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+    ssr: {
+        noExternal: ['@inertiajs/server'],
+    },
+    server: {
+        host: 'livia.5oaqkapv5e-e9249vmxw3kr.p.temp-site.link'
+    }
+});
